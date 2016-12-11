ee727b6d7527524b50fced6b85b8b8ba57537115
Real Limoges <b.real.limoges@gmail.com>
2016-12-11 05:25:44 -0800

    add basic signin credentials

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
-if __name__ == '__main__'
+with open('../config/access_token') as f:
+    access_token = f.read().strip()
+
+with open('../config/access_secret') as f:
+    access_secret = f.read().strip()
+
+with open('../config/consumer_key') as f:
+    consumer_key = f.read().strip()
+
+with open('../config/consumer_secret') as f:
+    consumer_secret = f.read().strip()
+
+
+if __name__ == '__main__':
     print "Hello World!"
