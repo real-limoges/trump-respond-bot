778dbda69ac973c4c6c48bc2dfb8c91af6a454ac
Real Limoges <b.real.limoges@gmail.com>
2016-12-14 21:13:38 -0800

    Update listener

--- src/DonaldListener.py
+++ src/DonaldListener.py
@@ -1,13 +1,18 @@
 import tweepy
-
+from auth_config import get_auth
 
 class DonaldListener(tweepy.StreamListener):
 
     def on_status(self, status):
         if '@realdonaldtrump' in status.text.encode('ascii', 'ignore').lower().replace(' ',''):
             pass
+        elif status.text.encode('ascii', 'ignore').lower().replace(' ','').startswith('rt'):
+            pass
         else:
             print status.text
+            api = tweepy.API( get_auth() )
+            api.update_status('testing')
+
 
     def on_error(self, status_code):
         print status_code
--- src/main.py
+++ src/main.py
@@ -3,12 +3,20 @@ import tweepy
 from DonaldListener import DonaldListener
 from auth_config import get_auth
 
+import time
+
+from requests.packages.urllib3.exceptions import ReadTimeoutError
+
 if __name__ == '__main__':
     auth = get_auth()
     donaldListener = DonaldListener()
-
+    
     tweet_stream = tweepy.Stream(auth=auth, listener=donaldListener)
     #Trump ID - public info
-    tweet_stream.filter(follow=['25073877'])
-    #Personal ID - public info
+    try:
+        tweet_stream.filter(follow=['25073877'])
+    except ReadTimeoutError:
+        time.sleep(10) 
+        tweet_stream.filter(follow=['20573877'])
+#    Personal ID - public info
 #    tweet_stream.filter(follow=['741352065289486337'])
