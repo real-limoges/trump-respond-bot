414a5e9e1a3e9316e756420956fc09b4e728b75e
Real Limoges <real@Reals-MacBook-Pro.local>
2016-12-26 13:40:26 -0800

    update with pylint and autopep8 code

--- src/DonaldListener.py
+++ src/DonaldListener.py
@@ -1,18 +1,28 @@
+'''
+Sets up the tweepy class listener object for streaming
+'''
+
 import tweepy
 from auth_config import get_auth
 
-class DonaldListener(tweepy.StreamListener):
 
+class DonaldListener(tweepy.StreamListener):
+    '''
+    Creates a donald trump listener. Could be broken into a setup where the
+    handle is passed but this project is narrow in scope
+    '''
     def on_status(self, status):
-        if '@realdonaldtrump' in status.text.encode('ascii', 'ignore').lower().replace(' ',''):
+        if '@realdonaldtrump' in status.text.encode(
+                'ascii', 'ignore').lower().replace(' ', ''):
             pass
-        elif status.text.encode('ascii', 'ignore').lower().replace(' ','').startswith('rt'):
+        elif status.text.encode(
+                'ascii', 'ignore').lower().replace(
+                    ' ', '').startswith('rt'):
             pass
         else:
             print status.text
-            api = tweepy.API( get_auth() )
+            api = tweepy.API(get_auth())
             api.update_status('testing')
-
 
     def on_error(self, status_code):
         print status_code
--- src/main.py
+++ src/main.py
@@ -1,22 +1,31 @@
+'''
+This module runs the code to setup a twitter stream, create a listener
+objet and handle basic exceptions
+'''
+import time
 import tweepy
 
 from DonaldListener import DonaldListener
 from auth_config import get_auth
 
-import time
-
 from requests.packages.urllib3.exceptions import ReadTimeoutError
 
+DONALD = '25073877'
+
 if __name__ == '__main__':
-    auth = get_auth()
-    donaldListener = DonaldListener()
-    
-    tweet_stream = tweepy.Stream(auth=auth, listener=donaldListener)
-    #Trump ID - public info
+    twitter_authorization = get_auth()
+    donald_trump_stream = DonaldListener()
+
+    real_time_tweets = tweepy.Stream(
+        auth=twitter_authorization,
+        listener=donald_trump_stream)
+
+    # Trump ID - public info
     try:
-        tweet_stream.filter(follow=['25073877'])
+        real_time_tweets.filter(follow=[DONALD])
     except ReadTimeoutError:
-        time.sleep(10) 
-        tweet_stream.filter(follow=['20573877'])
+        time.sleep(10)
+        real_time_tweets.filter(follow=[DONALD])
+
 #    Personal ID - public info
 #    tweet_stream.filter(follow=['741352065289486337'])
